42c28599849ebb85e84c9d0ead73f51017f243ba
Marcos Martins <30351698+marcosmn@users.noreply.github.com>
2018-09-03 15:15:18 -0300

    Update NewUser.js

---
+++ src/components/NewUser/NewUser.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import Label from '../Label';
+
+class NewUser extends Component {
+  render() {
+    return (
+      <div className="center">
+        <form className="pure-form pure-form-stacked">
+          <p>Quem é você</p>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NewUser;
